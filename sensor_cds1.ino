f76e08d4f3f359adc743967cd03f0dbd17784f33
Wolinam <zmffhqj35@naver.com>
2020-02-04 22:39:21 +0900

    Add files via upload

---
+++ sensor_cds1.ino
@@ -0,0 +1,24 @@
+int analoglnPin=A0;
+int analogOutPin=9;
+int sensorValue=0;
+int outputValue=0;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  sensorValue=analogRead(analoglnPin);
+  outputValue=map(sensorValue,0,1023,255,0);
+
+  if(outputValue<80) outputValue=0;
+
+  analogWrite(analogOutPin, outputValue);
+  Serial.print("sensor=");
+  Serial.print(sensorValue);
+  Serial.print("output=");
+  Serial.println(outputValue);
+
+  delay(1000);
+}
+
---
+++ sensor_ultrasonic1.ino
@@ -0,0 +1,28 @@
+int trigPin=9;
+int echoPin=8;
+void setup(){
+  Serial.begin(9600);
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+}
+
+
+void loop(){
+  float duration, distance;
+  digitalWrite(trigPin,HIGH);
+  delay(10);
+  digitalWrite(trigPin,LOW);
+
+  duration=pulseIn(echoPin,HIGH);
+
+  distance=((float)(340*duration)/10000)/2;
+
+  Serial.print("Duration");
+  Serial.print(duration);
+  Serial.println("Distance");
+  Serial.print(distance);
+  Serial.println("cm");
+  delay(500);
+ 
+}
+
